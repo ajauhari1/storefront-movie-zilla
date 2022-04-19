ce5431ed899c4ca0e2799ff6ec5129da826653ee
Abhishek Jauhari <65052823+ajauhari1@users.noreply.github.com>
2022-04-18 23:02:04 -0600

    updated code

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import '../styles/globalStyles.scss'
+
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,52 @@
-import {PageTitle} from './../components/PageTitle'
-export default function Home() {
-  // JSON Array of Objects 
-  // Array will be our Firebase data...
-  // take 3 products... and display them 
-  // add stripe
-  // create payment button
-  // api routes, ssr, isr... 
-  // getStaticPaths getStaticPages
+import Head from 'next/head'
+import PageTitle from "../components/PageTitle/PageTitle";
+import ProductCard from "../components/ProductCard/ProductCard";
+import {pane} from "./../styles/home.module.scss"
 
-  return (
-    <>
-    <header>
-      <PageTitle title="StoreFront" tagline="featured products"/>
-      <ul>
-        
-      </ul>
-    </header>
-    </>
-  )
+// https://shoeshine8k-default-rtdb.firebaseio.com/products.json
+/* 
+          SSG Static Site Generation
+          content
+          data + comp ======> html+css-----------------> edge/CDN
+
+*/
+ 
+export default function Home(props) {
+
+    const products = props.products.slice(0,3);
+   
+
+
+     return(
+          <>
+          <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <meta name="description" content="storefront online running shoes with free shipping"/>
+          <meta name="keywords" content="Shoes, Runing, Running Shoes, Nike Shoes, New Balance Shoes"/>
+           <title>Storefront</title>
+          </Head>
+           <PageTitle tagline="product specials" title="Storefront"/>
+           <main className={pane}>
+               {  products.map(product=> <ProductCard  key={product.uid} product={product}/>)}
+           </main>
+          </>
+     )
 }
+
+
+ 
+
+export async function getStaticProps(){
+  
+    const res = await fetch('https://storefront-933b9-default-rtdb.firebaseio.com/products.json')
+    const productData = await res.json();
+    const products = Object.values(productData)
+ return {
+      props:{
+           products
+      },
+      revalidate: 60,
+ }
+}
+
+ 
